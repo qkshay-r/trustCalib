fc1569137833b027acf4d914576b75ce235bbe07
qkshay-r <akshayrnair2001@gmail.com>
2026-03-13 13:43:01 +0530

    Add Render

--- server.ts
+++ server.ts
@@ -181,8 +181,8 @@ async function startServer() {
     });
   }
 
-  const PORT = 3000;
-  app.listen(PORT, "0.0.0.0", () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(Number(PORT), "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 }
